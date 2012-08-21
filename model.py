f1b22fc9f66d5b605594bfbdcd382ccb6dca712a
HVN <hvnsweeting@gmail.com>
2012-08-21 18:03:17 +0700

    update

--- model.py
+++ model.py
@@ -1,14 +1,27 @@
 import web
 import datetime
 import psycopg2
-import dj_database_url
-import os
 
 #postgres://mgzdqjxltjjfbs:TEoACC6-iEkBi8KBudnK3MAI2y@ec2-107-22-163-230.compute-1.amazonaws.com:5432/d8g05oiv7f32o2
-default=os.environ.get('DATABASE_URL')
 
+def dburl2dict(url):
+    dbn, rest = url.split('://', 1)
+    user, rest = rest.split(':', 1)
+    pw, rest = rest.split('@', 1)
+    host, rest = rest.split(':', 1)
+    port, rest = rest.split('/', 1)
+    db = rest
+    return dict(dbn=dbn, user=user, pw=pw, db=db, host=host)
 
-db = web.database("postgres://mgzdqjxltjjfbs:TEoACC6-iEkBi8KBudnK3MAI2y@ec2-107-22-163-230.compute-1.amazonaws.com:5432/d8g05oiv7f32o2")
+d = dburl2dict("postgres://mgzdqjxltjjfbs:TEoACC6-iEkBi8KBudnK3MAI2y@ec2-107-22-163-230.compute-1.amazonaws.com:5432/d8g05oiv7f32o2")
+
+db = web.database(dbn=d['dbn'], 
+                    db=d['db'],
+                    user=d['user'],
+                    pw=d['pw'],
+                    host=d['host'],
+                    port=d['port']
+                    )
 
 def get_tasks():
 	return db.select('tasks', order='enddate, priority DESC')
