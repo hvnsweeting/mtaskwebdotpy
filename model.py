abc67940134326d433c26ad64e1691b991272d46
HVN <hvnsweeting@gmail.com>
2012-08-21 17:47:26 +0700

    use url for db

--- model.py
+++ model.py
@@ -5,10 +5,10 @@ import dj_database_url
 import os
 
 #postgres://mgzdqjxltjjfbs:TEoACC6-iEkBi8KBudnK3MAI2y@ec2-107-22-163-230.compute-1.amazonaws.com:5432/d8g05oiv7f32o2
-db = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+default=os.environ.get('DATABASE_URL')
 
 
-#db = web.database(dbn='postgres', db='HEROKU_POSTGRESQL_CHARCOAL', user='mgzdqjxltjjfbs:', pw='TEoACC6-iEkBi8KBudnK3MAI2y' )
+db = web.database(default)
 
 def get_tasks():
 	return db.select('tasks', order='enddate, priority DESC')
