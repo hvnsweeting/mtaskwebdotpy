fac87c4398f9088e4b81744e0c3a477410485325
HVN <hvnsweeting@gmail.com>
2012-08-21 17:18:17 +0700

    add db info

--- model.py
+++ model.py
@@ -3,7 +3,9 @@ import datetime
 import psycopg2
 
 #DB
-db = web.database(dbn='postgres', db='mtask', )
+#postgres://mgzdqjxltjjfbs:TEoACC6-iEkBi8KBudnK3MAI2y@ec2-107-22-163-230.compute-1.amazonaws.com:5432/d8g05oiv7f32o2
+
+db = web.database(dbn='postgres', db='HEROKU_POSTGRESQL_CHARCOAL', user='mgzdqjxltjjfbs:', pw='TEoACC6-iEkBi8KBudnK3MAI2y' )
 
 def get_tasks():
 	return db.select('tasks', order='enddate, priority DESC')
