64e389af9e0cd5d60af3ac6c6aa11d4db1e64946
HVN <hvnsweeting@gmail.com>
2012-08-21 17:55:37 +0700

    update

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ import os
 default=os.environ.get('DATABASE_URL')
 
 
-db = web.database('postgres://mgzdqjxltjjfbs:TEoACC6-iEkBi8KBudnK3MAI2y@ec2-107-22-163-230.compute-1.amazonaws.com:5432/d8g05oiv7f32o2')
+db = web.database("postgres://mgzdqjxltjjfbs:TEoACC6-iEkBi8KBudnK3MAI2y@ec2-107-22-163-230.compute-1.amazonaws.com:5432/d8g05oiv7f32o2")
 
 def get_tasks():
 	return db.select('tasks', order='enddate, priority DESC')
